3de00402a86ce3e78cdaff79ce0a5e161188cf3a
jwaoluckas <jwaoluckas35@gmail.com>
2026-09-22 21:36:13 -0300

    Main monta o pedido do enunciado:
    - Bolo de chocolate simples.
    - Bolo de baunilha com o dizer "PLAIN!".
    - Bolo de baunilha com granulado e o dizer "FANCY!".
    - Bolo de morango multicamadas com granulado duas vezes e os dizeres
      "One of" e "EVERYTHING", mostrando as decorações empilhadas.
    - Order e Cake continuam sem nenhuma mudança.

--- Main.java
+++ Main.java
@@ -5,8 +5,15 @@ public class Main {
     public static void main(String[] args) {
         // Create the order
         Order order = new Order();
-        order.addCake(new VanillaCake());
         order.addCake(new ChocolateCake());
+        order.addCake(new SayingDecorator(new VanillaCake(), "PLAIN!"));
+        order.addCake(new SayingDecorator(new SprinklesDecorator(new VanillaCake()), "FANCY!"));
+        order.addCake(new MultiLayerDecorator(
+                new SayingDecorator(
+                        new SayingDecorator(
+                                new SprinklesDecorator(new SprinklesDecorator(new StrawberryCake())),
+                                "One of"),
+                        "EVERYTHING")));
 
         // Print the order
         order.printOrder();
